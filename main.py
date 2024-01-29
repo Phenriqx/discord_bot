6040e8f08f66118e61f8b1f0e0389862da6db99b
pedrohenrique <pedroxlbd@gmail.com>
2024-01-29 01:04:10 -0300

    testing commands

--- main.py
+++ main.py
@@ -1,30 +1,49 @@
 import os
+import sys
 import discord
+import random
 from dotenv import load_dotenv
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        for guild in client.guilds:
-            if guild.name == GUILD:
-                break
-            
-        print(
-            f'{client.user} is connected to the following guild:\n'
-            f'{guild.name}(id: {guild.id})'
-        )
-        
-        members = '\n - '.join([member.name for member in guild.members])
-        print(f'Guild Members:\n - {members}')
 
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+@bot.command(name='99')
+async def on_message(ctx):
+    # testing the command function
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
 
-intents = discord.Intents.default()
-intents.message_content = True
+    
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
 
-client = MyClient(intents=intents)
-client.run(TOKEN)
+
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+    
+async def on_error(event, *args, **kwargs):
+    with open('err.log') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}/n')
+        else:
+            raise sys.exc_info()
+
+
+bot.run(TOKEN)
