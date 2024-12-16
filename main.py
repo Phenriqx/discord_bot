c0db2055c1e4d4c3f8dacc0d36e3ae29ea369d14
pedrohenrique <pedroxlbd@gmail.com>
2024-12-16 00:32:39 -0300

    testing funcionalities

--- main.py
+++ main.py
@@ -15,26 +15,41 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 
-# @bot.command(name='99', help='This command shows random quotes from b99')
-# async def on_message(ctx):
-#     # testing the command function
-#     brooklyn_99_quotes = [
-#         'I\'m the human form of the 💯 emoji.',
-#         'Bingpot!',
-#         (
-#             'Cool. Cool cool cool cool cool cool cool, '
-#             'no doubt no doubt no doubt no doubt.'
-#         ),
-#     ]
-
-    
-#     response = random.choice(brooklyn_99_quotes)
-#     await ctx.send(response)
-
 @bot.event
 async def on_ready():
-    print(f'Hello {bot.user}!')
+   print(f'Hello {bot.user}!')
 
+
+# def to_upper(argument):
+#    return argument.upper()
+
+#@bot.command()
+#async def up(ctx, *, content: to_upper):
+#    await ctx.send(content)
+
+class JoinDistance:
+    def __init__(self, joined, created):
+        self.joined = joined
+        self.created = created
+
+    @classmethod
+    async def convert(cls, ctx, argument):
+        member = await commands.MemberConverter().convert(ctx, argument)
+        return cls(member.joined_at, member.created_at)
+
+    @property
+    def delta(self):
+        return self.joined - self.created
+
+@bot.command()
+async def delta(ctx, *, member: JoinDistance):
+    is_new = member.delta.days < 100
+    print(member.delta.days)
+    if is_new:
+        await ctx.send("Hey you're pretty new!")
+    else:
+        await ctx.send("Hm you're not so new.")
+        
 
 @bot.command('roll_dice')
 async def roll(ctx, number_of_dice: int, number_of_sides: int, ):
@@ -42,8 +57,9 @@ async def roll(ctx, number_of_dice: int, number_of_sides: int, ):
         str(random.choice(range(1, number_of_sides + 1)))
         for _ in range(number_of_dice)
     ]
-
+    
     await ctx.send(', '.join(dice))
+
 
 @bot.command('create_voice_channel', help='Create a voice channel')
 @commands.has_role('admin')
@@ -52,7 +68,7 @@ async def create_voice_channel(ctx, channel_name):
     existing_channel = discord.utils.get(guild.channels, name=channel_name)
     if not existing_channel and channel_name != None:
         await guild.create_voice_channel(channel_name)
-        await ctx.send(f'Voice channel created as {channel_name}')
+        await ctx.send(f'Voice channel created as {channel_name} by {ctx.author}')
         
 
 @bot.command('create_text_channel', help='Create a text channel')
@@ -62,7 +78,7 @@ async def create_text_channel(ctx, channel_name):
     existing_channel = discord.utils.get(guild.channels, name=channel_name)
     if not existing_channel and channel_name != None:
         await guild.create_text_channel(channel_name)
-        await ctx.send(f'Text channel created as {channel_name}')
+        await ctx.send(f'Text channel created as {channel_name} by {ctx.author}')
         
         
 @bot.command(name='delete-channel', help='delete a channel with the specified name')
